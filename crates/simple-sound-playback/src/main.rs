7d753bef64394c3f59c045e44828c0df835d4125
Andrew Minnich <aminnich3@gmail.com>
2023-05-20 17:18:48 -0400

    simplify the simple sound playback example

--- crates/simple-sound-playback/src/main.rs
+++ crates/simple-sound-playback/src/main.rs
@@ -4,21 +4,15 @@ use kira::{
 	manager::{backend::DefaultBackend, AudioManager, AudioManagerSettings},
 	sound::static_sound::{StaticSoundData, StaticSoundSettings},
 };
-use rand::{thread_rng, Rng};
 
 fn main() -> Result<(), Box<dyn Error>> {
 	let mut manager = AudioManager::<DefaultBackend>::new(AudioManagerSettings::default())?;
 	let sound_data = StaticSoundData::from_file("assets/blip.ogg", StaticSoundSettings::default())?;
-	let mut rng = thread_rng();
 
 	println!("Press enter to play a sound");
 	loop {
 		wait_for_enter_press()?;
-		let sound = sound_data.with_modified_settings(|s| {
-			s.volume(rng.gen_range(0.5..=1.0))
-				.playback_rate(rng.gen_range(0.5..=1.5))
-		});
-		manager.play(sound)?;
+		manager.play(sound_data.clone())?;
 	}
 }
 
